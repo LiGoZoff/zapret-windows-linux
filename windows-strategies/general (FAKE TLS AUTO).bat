c6b73de2c09bcde5874f0f2150121cadf4ffbd88
LiGoZoff <0718snak0718@gmail.com>
2025-11-27 22:13:07 +0300

    Update general (FAKE TLS AUTO).bat

--- windows-strategies/general (FAKE TLS AUTO).bat
+++ windows-strategies/general (FAKE TLS AUTO).bat
@@ -2,14 +2,14 @@
 chcp 65001 > nul
 :: 65001 - UTF-8
 
-cd /d "%~dp0"
+cd /d "%~dp0.."
 call service.bat status_zapret
 call service.bat check_updates
 call service.bat load_game_filter
 echo:
 
-set "BIN=%~dp0bin\"
-set "LISTS=%~dp0lists\"
+set "BIN=%~dp0..\bin\"
+set "LISTS=%~dp0..\lists\"
 cd /d %BIN%
 
 start "zapret: %~n0" /min "%BIN%winws.exe" --wf-tcp=80,443,2053,2083,2087,2096,8443,%GameFilter% --wf-udp=443,19294-19344,50000-50100,%GameFilter% ^
